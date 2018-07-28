2a4d108bf7383371448a9f1d9c06fa78d81b4d10
Rohtash Singh <rohtash.singh@kiwitech.com>
2018-07-28 19:54:33 +0530

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component'; 
+import { HomeComponent} from './home/home.component';
+import { ServicesComponent} from './services/services.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
-const routes: Routes = [];
+ 
+const routes: Routes = [
+  { path:'about.html',component:AboutUsComponent},
+  { path:'home.html',component:HomeComponent},
+  { path:'services.html',component:ServicesComponent},
+  { path:'gallery.html',component:GalleryComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const RoutingComponents = ['AboutUsComponent'];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AboutUsComponent } from './about-us/about-us.component'; 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    HomeComponent,
+    ServicesComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
